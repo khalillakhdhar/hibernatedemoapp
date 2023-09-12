58f105ed34c4f631d478ca3f704a35861db159e5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-09-12 20:00:05 +0100

    Initial commit

---
+++ src/main/java/com/hibernate/model/Student.java
@@ -0,0 +1,65 @@
+package com.hibernate.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "etudiant")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false, length = 30)
+	private String nom,prenom;
+	@Column(nullable = false)
+	private String mdp;
+	@Column(unique = true)
+	private String email;
+	
+	private int age;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", mdp=" + mdp + ", age=" + age + "]";
+	}
+	
+	
+	
+	
+
+}
